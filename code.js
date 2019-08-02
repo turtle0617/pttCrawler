84edecbce6de8d8c0a1f01d24ef49f9a591a43b1
張睿峰 <qqaz50313@gmail.com>
2019-08-02 15:18:54 +0800

    [Fix] remove test js

--- code.js
+++
@@ -1,8 +0,0 @@
-const recognize = require('tesseractocr')
-
-recognize(`${__dirname}/getCode.tif`, (err, text) => {
-    if (err)
-        console.log(err)
-    else
-        console.log('Yay! Text recognized!', text)
-})
--- index.js
+++ index.js
@@ -1,26 +1,80 @@
-var webdriver = require('selenium-webdriver');
-var chrome = require('selenium-webdriver/chrome');
-var path = require('chromedriver').path;
+const request = require('request');
+const cheerio = require('cheerio');
+const nodemailer = require('nodemailer');
+var cron = require('node-cron');
 
-var service = new chrome.ServiceBuilder(path).build();
-chrome.setDefaultService(service);
+const mobilesales_URL = 'https://www.ptt.cc/bbs/mobilesales/index.html';
+const crontab_period = 60 * 5;
 
-var driver = new webdriver.Builder()
-  .withCapabilities(webdriver.Capabilities.chrome())
-  .build();
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'qqaz50313@gmail.com',
+    pass: '',
+  },
+});
+
+let task = cron.schedule('*/5 * * * *', () => {
+  console.log('running every 5 minute');
+  request(mobilesales_URL, mobilesales);
+});
+task.start();
+// request(mobilesales_URL, mobilesales);
+
+function mobilesales(error, response, body) {
+  let $ = cheerio.load(body);
+  let post_length = $('.title').length;
+  let post_list = $('.title a').map((index, obj) => {
+    if (index < post_length - 3) {
+      let timestamp = parseInt($(obj).attr('href').substr(19, 10));
+      return {
+        title: $(obj).text(),
+        url: "https://www.ptt.cc" + $(obj).attr('href'),
+        timestamp: timestamp,
+      }
+    }
+    return
+  }).get()
+
+  post_list = post_list.filter(isIphone7);
+  // console.log(post_list);
+  sendMail(post_list);
+}
+
+function isIphone7(post) {
+  let within_crontab_period = post.timestamp > (Date.now() / 1000 - crontab_period);
+  if (within_crontab_period) {
+    return !!post.title.match(/(iphone)/i) && !!post.title.match(/7/i);
+
+;
+  }
+}
+
+function sendMail(list) {
+  let list_length = list.length;
+  if (!list_length) {
+    console.log("沒有新文章！！");
+    return
+  }
+  console.log("寄送信件中...");
+  const mailOptions = {
+    from: 'qqaz50313@gmail.com',
+    to: 'qqaz50313@gmail.com',
+    subject: list[list_length - 1].title,
+    text: list[list_length - 1].url,
+  };
+  // console.log(mailOptions);
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
 
 
-(async function name() {
-  await driver.get('https://event.mi.com/tw/sales2018/super-sales-day/?utm_source=pc');
-  await driver.findElement({ xpath: '//*[@id="sec_1"]/div/div/ul/li/ol/li[1]/a/span' }).click();
-  await driver.findElement({ xpath: '//*[@id="username"]' }).sendKeys('qqaz50313@gmail.com');
-  await driver.findElement({ xpath: '//*[@id="pwd"]' }).sendKeys('');
-  // await driver.findElement({ xpath: '//*[@id="login-button"]' }).click();
-  setTimeout(function () {
-     driver.findElement({ xpath: '//*[@id="login-button"]' }).click();
-     setTimeout(function () {
-        driver.findElement({ xpath: '//*[@id="sec_1"]/div/div/ul/li/ol/li[1]/a/span' }).click();
-     }, 1000);
-  }, 5000);
-
-})();
+function sliceStr(str, index) {
+  let strReplace = str.replace(/\s/g, "");
+  return strReplace.slice(0, strReplace.indexOf("搜尋同標題文章"))
+}
